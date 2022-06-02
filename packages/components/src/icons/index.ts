1ec5321ce51fae15d19e421819a4d2ec280fd112
macintoshhelper <macintoshhelper@users.noreply.github.com>
2022-06-02 20:43:42 +0400

    create zcash heart icon

--- packages/components/src/icons/index.ts
+++ packages/components/src/icons/index.ts
@@ -1,8 +1,10 @@
 export { default as CloseIcon } from './CloseIcon';
 export { default as RepostIcon } from './RepostIcon';
 export { default as ReplyIcon } from './ReplyIcon';
+export { default as LinkIcon } from './LinkIcon';
 export { FavoriteIcon, ShareIcon } from './icons';
 // export { default as ShareIcon } from './ShareIcon';
 export { default as ZcashIcon } from './ZcashIcon';
+export { default as ZcashHeartIcon } from './ZcashHeartIcon';
 
 

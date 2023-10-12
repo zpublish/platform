c4705f8e4dac7a672590a224f28bd2f6eb8e7665
macintoshhelper <macintoshhelper@users.noreply.github.com>
2023-10-12 20:17:14 +0100

    web app common components and scaffolding

---
+++ packages/web/lib/constants.ts
@@ -0,0 +1,8 @@
+export const STORAGE_KEYS = {
+  ACCESS_TOKEN: 'accessToken',
+  CODE_VERIFIER: 'codeVerifier',
+  EXPIRES_AT: 'expiresAt',
+  REFRESH_TOKEN: 'refreshToken',
+  STATE: 'state',
+} as const;
+

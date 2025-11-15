325164aee2ab7954d8942da9f94aea8fbca4b662
macintoshhelper <6757532+macintoshhelper@users.noreply.github.com>
2025-11-15 21:05:30 +0400

    update tailwind gradient vars

--- packages/web/tailwind.config.ts
+++ packages/web/tailwind.config.ts
@@ -80,6 +80,10 @@ const config: Config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        'gradient': "var(--gradient)",
+        'gradient-dark': "var(--gradient-dark)",
+      }
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
